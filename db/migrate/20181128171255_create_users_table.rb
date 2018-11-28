2a421036614bef4935f8e7e2ca73dc2ffeb643a1
oky-123 <oky123.ia@gmail.com>
2018-11-29 02:35:50 +0900

    Add migration

---
+++ db/migrate/20181128171255_create_users_table.rb
@@ -0,0 +1,13 @@
+class CreateUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :crypted_password
+      t.string :salt
+      t.unsigned_integer :university_id
+      t.unsigned_integer :faculty_id
+      t.unsigned_integer :age, limit: 1
+    end
+  end
+end
