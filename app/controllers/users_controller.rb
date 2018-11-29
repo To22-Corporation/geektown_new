54f9cbb774ec0b9254d74e901569bddb89688d02
oky-123 <oky123.ia@gmail.com>
2018-11-30 04:32:42 +0900

    Fix user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: %i[show edit update]
 
   # GET /users
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resource :registration, only: %i[new create]
-  get 'login' => 'user_sessions#new'
-  post 'login' => 'user_sessions#create', as: :post_login
+  get 'login' => 'user_sessions#new', as: :new_login
+  post 'login' => 'user_sessions#create'
   post 'logout' => 'user_sessions#destroy'
 
   resources :users, except: %i[new create destroy]
