65a5abda1595e761fe647ad833c8c6de157d6927
oky-123 <oky123.ia@gmail.com>
2018-12-04 23:25:05 +0900

    Add seed data

---
+++ db/fixtures/faculties.rb
@@ -0,0 +1,54 @@
+Faculty.seed do |s|
+  s.id = 1
+  s.name = '工学部'
+end
+
+Faculty.seed do |s|
+  s.id = 2
+  s.name = '理学部'
+end
+
+Faculty.seed do |s|
+  s.id = 3
+  s.name = '農学部'
+end
+
+Faculty.seed do |s|
+  s.id = 4
+  s.name = '医学部'
+end
+
+Faculty.seed do |s|
+  s.id = 5
+  s.name = '薬学部'
+end
+
+Faculty.seed do |s|
+  s.id = 6
+  s.name = '歯学部'
+end
+
+Faculty.seed do |s|
+  s.id = 7
+  s.name = '看護学部'
+end
+
+Faculty.seed do |s|
+  s.id = 8
+  s.name = '経済学部'
+end
+
+Faculty.seed do |s|
+  s.id = 9
+  s.name = '文学部・人文学部'
+end
+
+Faculty.seed do |s|
+  s.id = 10
+  s.name = '教育学部'
+end
+
+Faculty.seed do |s|
+  s.id = 11
+  s.name = '外国語学部'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_140920) do
+ActiveRecord::Schema.define(version: 2018_12_04_141357) do
 
   create_table "faculties_tables", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "universities_tables", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
   end
 
