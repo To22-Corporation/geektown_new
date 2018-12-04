58e0fc4c0397cd9f6b788f5ba5699fe35d1673da
oky-123 <oky123.ia@gmail.com>
2018-12-05 01:54:08 +0900

    Add redirect filter

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Users::ProfilesController < ApplicationController
-  before_action :has_profile, only: %i[new create]
+  before_action :profile?, only: %i[new create]
+  before_action :no_profile?, only: %i[edit update]
 
   def new
-    @universities = University.all
-    @faculties = Faculty.all
+    set_master
     @profile = Profile.new
   end
 
@@ -20,8 +20,7 @@ class Users::ProfilesController < ApplicationController
   end
 
   def edit
-    @universities = University.all
-    @faculties = Faculty.all
+    set_master
     @profile = current_user.profile
   end
 
@@ -43,9 +42,16 @@ class Users::ProfilesController < ApplicationController
 
   private
 
-  def has_profile
-    if current_user.profile
-      redirect_to action: :edit, user_id: current_user.id
-    end
+  def profile?
+    redirect_to action: :edit, user_id: current_user.id if current_user.profile
+  end
+
+  def no_profile?
+    redirect_to action: :new unless current_user.profile
+  end
+
+  def set_master
+    @universities = University.all
+    @faculties = Faculty.all
   end
 end
