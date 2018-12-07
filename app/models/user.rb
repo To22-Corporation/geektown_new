198ef25d71fcf71e5307ef02ed13e3072c481268
oky-123 <oky123.ia@gmail.com>
2018-12-08 02:21:40 +0900

    Fix migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_one :profile, dependent: :destroy
-  has_many :group_skills, dependent: :destroy
-  has_many :groups, through: :group_skills
+  has_many :participations, dependent: :destroy
+  has_many :groups, through: :participations
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
