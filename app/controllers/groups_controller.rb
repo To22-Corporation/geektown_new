bd32267bd21a277b65db59079fbf5384c64013c0
oky-123 <oky123.ia@gmail.com>
2018-12-18 18:41:03 +0900

    Add datetimepicker

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,15 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+  end
 end
