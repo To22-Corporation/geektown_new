fa4954054c21c7046e52aa620186ebdd5f3cceef
oky-123 <oky123.ia@gmail.com>
2018-12-19 18:46:01 +0900

    Fix model relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      current_user.participations.create(group_id: @group.id, is_owner: true)
+      @group.build_ownership(user_id: current_user.id).save
       redirect_to({ action: 'edit', id: @group.id }, notice: 'グループを作成しました')
     else
       render :new
