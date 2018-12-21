add03b5e13631165d448dbe346e9a38e6b1a5117
oky-123 <oky123.ia@gmail.com>
2018-12-22 07:52:52 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   resources :users, module: :users, only: %i[index] do
     resource :profile, only: %i[new create edit update show]
   end
-  resources :groups, module: :groups, only: %i[new create edit update index show] do
-    resources :messages, only: %i[index create]
+
+  resources :groups, only: %i[new create edit update index show] do
+    scope module: :groups do
+      resources :messages, only: %i[index create]
+    end
   end
 end
