7626d49905f83f1699ad34d8521faec60d2551a0
oky-123 <oky123.ia@gmail.com>
2018-12-22 07:34:50 +0900

    Add view

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
+  has_one :message_participation, dependent: :destroy
+  has_one :participation, through: :message_participation
+  has_one :user, through: :participation
+
   validates :content, presence: true, length: { minimum: 1 }
 end
