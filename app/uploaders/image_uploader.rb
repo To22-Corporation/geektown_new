b49ba460fd9dca5cc9e88d8416b86d8011c1236d
oky-123 <oky123.ia@gmail.com>
2018-12-25 18:46:26 +0900

    Add imagemagic

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,35 +13,40 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  include CarrierWave::RMagick
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  process :resize_to_limit => [640, 480]
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  process :convert => 'jpg'
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  version :thumb do
+    process :resize_to_limit => [300, 300]
+  end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  version :thumb100 do
+    process :resize_to_limit => [100, 100]
+  end
+
+  version :thumb30 do
+    process :resize_to_limit => [30, 30]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
