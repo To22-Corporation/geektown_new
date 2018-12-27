a511c56cac6615468bcf0548b373c791d6ff1420
oky-123 <oky123.ia@gmail.com>
2018-12-28 03:34:37 +0900

    Add source

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,7 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  has_many :message_participations, dependent: :destroy
+  has_many :message_participations, dependent: :nullify
   has_many :messages, through: :message_participations
 
   validates :user, presence: true
