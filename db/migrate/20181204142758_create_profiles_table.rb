21d8161081bbd7e992f4c1c1db42579f198952a6
kskmt0414 <45933391+kskmt0414@users.noreply.github.com>
2019-01-13 13:50:34 +0900

    Update 20181204142758_create_profiles_table.rb

--- db/migrate/20181204142758_create_profiles_table.rb
+++ db/migrate/20181204142758_create_profiles_table.rb
@@ -3,8 +3,8 @@ class CreateProfilesTable < ActiveRecord::Migration[5.2]
     create_table :profiles do |t|
       t.string :name, null: false
       t.references :user, null: false, foreign_key: true
-      t.references :university, null: false, foreign_key: true
-      t.references :faculty, null: false, foreign_key: true
+#      t.references :university, null: false, foreign_key: true
+#      t.references :faculty, null: false, foreign_key: true
 
       t.timestamps
     end
