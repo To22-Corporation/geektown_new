0c7f32083bcbc3ad504a74aedc6829a245646e2a
kskmt0414 <45933391+kskmt0414@users.noreply.github.com>
2019-01-13 13:49:55 +0900

    Update 20181231090804_chage_profiles.rb

--- db/migrate/20181231090804_chage_profiles.rb
+++ db/migrate/20181231090804_chage_profiles.rb
@@ -1,10 +1,10 @@
 class ChageProfiles < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :profiles, :university, foreign_key: true
-    remove_reference :profiles, :faculty, foreign_key: true
-    add_column :profiles, :university, :string, null: true
-    add_column :profiles, :faculty, :string, null: true
+    #remove_reference :profiles, :university, foreign_key: true
+    #remove_reference :profiles, :faculty, foreign_key: true
+    #add_column :profiles, :university, :string, null: true
+    #add_column :profiles, :faculty, :string, null: true
     add_column :profiles, :comment, :string, null: true
-    add_column :profiles, :grade, :integer, null: true
+    #add_column :profiles, :grade, :integer, null: true
   end
 end
